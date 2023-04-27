90e8f6bdc1335b4340b01b1c90afcf512fa7a7d2
agastyathesuperkidwhocanflyrockets <72139130+agastyathesuperkidwhocanflyrockets@users.noreply.github.com>
2023-04-27 19:23:14 +0530

    Update Filter.py
    It's the tutorial. Not finished dataset yet.

--- Filter.py
+++ Filter.py
@@ -7,3 +7,16 @@ import tensorflow as tf
 
 from tensorflow.keras import layers
 from tensorflow.keras import losses
+url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+
+dataset = tf.keras.utils.get_file("aclImdb_v1", url,
+                                    untar=True, cache_dir='.',
+                                    cache_subdir='')
+
+dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+os.listdir(dataset_dir)
+train_dir = os.path.join(dataset_dir, 'train')
+os.listdir(train_dir)
+sample_file = os.path.join(train_dir, 'pos/1181_9.txt')
+with open(sample_file) as f:
+  print(f.read())
