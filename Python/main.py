8b3b11dadf9ab1142faa617d0dcb35cb72025442
venkatsushanthk <121592277+venkatsushanthk@users.noreply.github.com>
2023-04-28 10:02:38 +0530

    t

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,29 @@
+#
+# Jo-GPT
+#
 
+# Chat-GPT but more Jo mama and allows you to bully kids
+
+import time
+
+
+
+# Introduction
+print("My name is Jo-GPT.\n")
+time.sleep(2)
+print("I am Chat GPT but allows you to bully kids and is more Jo mama.\n")
+time.sleep(4.5)
+
+# Start of conversation
+user_input = input("How may i help you today?\n")
+
+if user_input == "where to walk a dog":
+    print("A park.\n")
+    answer = input("Did the answer satisfy your question?\n")
+    if answer == "no":
+        print("You are banned for 1 day. nobody says no to my answer.")
+        exit(all)
+    elif answer == "yes":
+        print("Thank you!")
+else:
+    print("I still get enough data to learn that.")
