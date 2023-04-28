4f79ef4f3598f7b5c8ddd28d25ea2e5c7856993e
venkatsushanthk <121592277+venkatsushanthk@users.noreply.github.com>
2023-04-28 09:24:07 +0530

    t

---
+++ python/main.py
@@ -0,0 +1,39 @@
+#
+# Jo-GPT
+#
+
+# Chat GPT but more JO mama because it tells jokes and stops you from being racist and bans your account from using Jo-GPT
+
+import time
+
+
+
+print("Hello my name is Jo-GPT.\n")
+time.sleep(2)
+print("Some people may not know what I am but I am a robot capable of")
+time.sleep(3.5)
+print("\ntelling you to bully kids and ban you from using this robot if I want.\n")
+time.sleep(4)
+
+user_input = input("How may I help you today?\n")
+if user_input == "I want to know places to take a dog for a walk":
+    print("A park.")
+    yes = input("Did you like the answer?")
+    if yes == "no":
+        print("Ok, you are banned nobody says no to my answer.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
